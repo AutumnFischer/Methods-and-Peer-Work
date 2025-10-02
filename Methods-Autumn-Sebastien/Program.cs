6bed1db52a12f78355b2a5f79e572e2f24b337ca
Sébastien Guillotin <sebgusa@gmail.com>
2025-10-02 08:53:16 -0400

    Add my piece
    Auto comment didn't work so I did it by hand like a big macho machine coder.

--- Methods-Autumn-Sebastien/Program.cs
+++ Methods-Autumn-Sebastien/Program.cs
@@ -14,14 +14,30 @@ namespace Methods_Autumn_Sebastien
 
             //initialize
             autumnFavoriteNumber = 13;
-            sebastienFavoriteNumber = 69;
+            sebastienFavoriteNumber = 67;
+
+            /// <summary>
+            ///     A funtion that takes two integers as parameters and returns their difference
+            /// </summary>
+            /// <param name="num1">the first number</param>
+            /// <param name="num2">the second number</param>
+            /// <returns>returns the two parameters subtracted</returns>
+            int Subtract(int num1, int num2)
+            {
+                int result = num1 - num2;
+                return result;
+            }
 
             //start the real stuff
             Console.WriteLine("Autumn's favorite number is " + autumnFavoriteNumber + "!");
+            Console.WriteLine("Sebastien's favorite number is " + sebastienFavoriteNumber + " :(");
             //add sebastien's print here
 
             int sum = Add(autumnFavoriteNumber, sebastienFavoriteNumber);
             Console.WriteLine("The sum of our favorite numbers is: " + sum + "!");
+
+            int difference = Subtract(sebastienFavoriteNumber, autumnFavoriteNumber);
+            Console.WriteLine("The difference between our favorite numbers is: " + difference + "! :D");
         }
 
         /// <summary>
