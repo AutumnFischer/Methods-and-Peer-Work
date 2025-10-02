fe89f02cecb15b7f4f8c48a1c8a38e448c500888
AutumnFischer <aef6952@rit.edu>
2025-10-02 08:17:43 -0400

    added vs file

---
+++ Methods-Autumn-Sebastien/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Methods_Autumn_Sebastien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
