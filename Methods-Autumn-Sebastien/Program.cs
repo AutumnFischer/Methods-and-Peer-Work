49a989e183e5d5578e02bc75c082ff9ae327cbf5
AutumnFischer <aef6952@rit.edu>
2025-10-02 08:33:05 -0400

    added "add" method and comments

--- Methods-Autumn-Sebastien/Program.cs
+++ Methods-Autumn-Sebastien/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace Methods_Autumn_Sebastien
+﻿/**
+ * Code written by Autumn Fischer and Sebastien Guillotin
+ * Assignment for IGME.201.01 - Interac Des & Alg Prob Sol III
+ * Due October 12, 2025
+**/
+namespace Methods_Autumn_Sebastien
 {
     internal class Program
     {
@@ -12,6 +17,23 @@
             sebastienFavoriteNumber = 69;
 
             //start the real stuff
+            Console.WriteLine("Autumn's favorite number is " + autumnFavoriteNumber + "!");
+            //add sebastien's print here
+
+            int sum = Add(autumnFavoriteNumber, sebastienFavoriteNumber);
+            Console.WriteLine("The sum of our favorite numbers is: " + sum + "!");
+        }
+
+        /// <summary>
+        ///     A funtion that takes two integers as parameters and returns their sum
+        /// </summary>
+        /// <param name="num1">the first number</param>
+        /// <param name="num2">the second number</param>
+        /// <returns>returns the two parameters added together</returns>
+        static int Add(int num1, int num2)
+            { 
+                int result = num1 + num2;
+                return result;
         }
     }
 }
